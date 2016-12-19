56108a9617483261b5275350b4a5ef336d749648
sabahjones <sabahjones@gmail.com>
2016-12-19 14:49:19 -0600

    updated billions.js

--- javascript/Sabah/billions.js
+++ javascript/Sabah/billions.js
@@ -1,9 +1,9 @@
 sum = .01;
-for (var days = 1; days < 31; days++){
+for (var days = 2; days < 31; days++){
     sum *=2;
 
-    if (sum > 10000){
+    /*if (sum > 10000){
       break;
-    }
+    }*/
 }
 console.log("it took the peasant " + days + " days to accumulate " + sum);

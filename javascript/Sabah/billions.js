df9bafef59e32ec9f4ffe34fdeb45f6209153988
sabahjones <sabahjones@gmail.com>
2016-12-19 14:42:04 -0600

    set loop in billions.js to terminate at 0,000

--- javascript/Sabah/billions.js
+++ javascript/Sabah/billions.js
@@ -1,5 +1,9 @@
 sum = .01;
 for (var days = 1; days < 31; days++){
     sum *=2;
+
+    if (sum > 10000){
+      break;
+    }
 }
-console.log(sum);
+console.log("it took the peasant " + days + " days to accumulate " + sum);

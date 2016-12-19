a50c2117847662e21d70e62a285a7794f75aa8ee
Jeremy Gillmore <happy12287@gmail.com>
2016-12-19 12:53:58 -0600

    finished while you wait (the birthday assignment)

---
+++ javascript/jeremyGillmore/While_you_wait.js
@@ -0,0 +1,17 @@
+for (var daysTilBirthday = 60; daysTilBirthday > 0; daysTilBirthday--)
+{
+  if (daysTilBirthday > 30)
+  {
+    console.log(daysTilBirthday, "days until my birthday. Such a long tim... :(");
+  }
+  else if (daysTilBirthday > 5) {
+    console.log(daysTilBirthday, "days until my birthday. its getting close :)");
+  }
+  else
+  {
+    console.log(daysTilBirthday, "DAYS UNTIL MY BIRTHDAY!!!!!!!!!");
+  }
+}
+console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+console.log("!!!!!    ITS MY BIRTHDAY     !!!!!!!!!!!!!    YEAH   !!!!!!!!");
+console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");

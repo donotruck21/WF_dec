ea36657df2ff496d1bb7b1132adc006f19fbd66e
Jeremy Gillmore <happy12287@gmail.com>
2016-12-21 15:04:13 -0600

    finished users and color-clickera ssignments

---
+++ testing_obj.js
@@ -0,0 +1,17 @@
+var obj = {
+  1:"I",
+  1000: "M",
+  5:"V",
+  900:"CM",
+  10:"X",
+  40:"XB",
+  50:"B",
+  9:"IX",
+  90:"XC",
+  100:"C",
+  4:"IV",
+  500:"L",
+  400:"CL"}
+for (var key in obj){
+  console.log(key);
+}
